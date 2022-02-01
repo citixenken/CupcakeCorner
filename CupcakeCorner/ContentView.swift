0c4b2ad18bab43a3065d8acc3ed3b41656fa9b6c
citixenken <kmuyesu@gmail.com>
2022-02-01 22:42:29 +0300

    loading image from remote server

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -26,50 +26,75 @@ import SwiftUI
 //    }
 //}
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
+//struct Response: Codable {
+//    var results: [Result]
+//}
+//
+//struct Result: Codable {
+//    var trackId: Int
+//    var trackName: String
+//    var collectionName: String
+//}
 
 
 struct ContentView: View {
-    @State private var results = [Result]()
-    
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=shaggy&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            //data object
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            //convert to Response object
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("Invalid data")
-        }
-    }
+//    @State private var results = [Result]()
+//
+//    func loadData() async {
+//        guard let url = URL(string: "https://itunes.apple.com/search?term=kendrick+lamar&entity=song") else {
+//            print("Invalid URL")
+//            return
+//        }
+//
+//        do {
+//            //data object
+//            let (data, _) = try await URLSession.shared.data(from: url)
+//
+//            //convert to Response object
+//            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+//                results = decodedResponse.results
+//            }
+//        } catch {
+//            print("Invalid data")
+//        }
+//    }
     
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
+        
+//        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
+//            image
+//                .resizable()
+//                .scaledToFit()
+//        } placeholder: {
+//            //Color.mint
+//            ProgressView()
+//        }
+//        .frame(width: 400, height: 400)
+        
+        AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("There was an error loading the image.")
+            } else {
+                ProgressView()
             }
         }
-        .task {
-            await loadData()
-        }
+        .frame(width: 300, height: 300)
+        
+        
+//        List(results, id: \.trackId) { item in
+//            VStack(alignment: .leading) {
+//                Text(item.trackName)
+//                    .font(.headline)
+//                Text(item.collectionName)
+//            }
+//        }
+//        .task {
+//            await loadData()
+//        }
     }
 }
 
