0910c6de8f6df90bfd8e0811696589e3966a7170
citixenken <kmuyesu@gmail.com>
2022-02-04 00:17:06 +0300

    checking for a valid address

--- Order.swift
+++ Order.swift
@@ -17,10 +17,23 @@ class Order: ObservableObject {
         didSet {
             if specialRequestEnabled == false {
                 extraFrosting = false
-                addSprinkles = false 
+                addSprinkles = false
             }
         }
     }
     @Published var extraFrosting = false
     @Published var addSprinkles = false
+    
+    @Published var name = ""
+    @Published var streetAddress = ""
+    @Published var city = ""
+    @Published var zip = ""
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+        
+        return true
+    }
 }
